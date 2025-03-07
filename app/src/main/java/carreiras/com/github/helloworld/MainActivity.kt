8808495233e98192db936daea1125ba6e4effb43
isabellesf <isabelleserrano.furman@gmail.com>
2025-03-06 21:25:51 -0300

    fases 1 e 2

---
+++ app/src/main/java/carreiras/com/github/helloworld/MainActivity.kt
@@ -0,0 +1,45 @@
+package carreiras.com.github.helloworld
+
+import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import carreiras.com.github.helloworld.ui.theme.HelloworldTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Associa a Activity ao layout definido em activity_main.xml
+        setContentView(R.layout.activity_main)
+
+        // Referências para os componentes do layout
+        val etName = findViewById<EditText>(R.id.etName)
+        val tvHelloMessage = findViewById<TextView>(R.id.tvHelloMessage)
+
+        // Listener para quando o usuário pressiona Enter (actionDone)
+        etName.setOnEditorActionListener { textView, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                val name = textView.text.toString().trim()
+                // Verifica se o nome foi digitado
+                if (name.isNotEmpty()) {
+                    tvHelloMessage.text = "Hello World, $name!"
+                } else {
+                    tvHelloMessage.text = "Hello World!"
+                }
+                true // Indica que consumimos o evento
+            } else {
+                false
+            }
+        }
+    }
+}
